03846292ccd5ba8b6aab3c7d35a6f242544cbb19
jcrrilloo <164308283+jcrrilloo@users.noreply.github.com>
2024-04-14 21:54:39 -0700

    Factorial Program

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int startingNumber = 4;
+        Console.Out.WriteLine(factorial(startingNumber));
+        Console.ReadLine();
+    }
+
+    static int factorial(int x)
+    {
+        Console.Out.WriteLine("x is {0}", x);
+        if ( x == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * factorial(x - 1);
+        }
+    }
+}
+
